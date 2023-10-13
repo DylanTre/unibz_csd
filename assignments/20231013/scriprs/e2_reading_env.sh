2006e2390d4a9835dc122338e95a360557d115d8
Dylan Trenti <dtrenti@fbk.eu>
2023-10-13 12:46:40 +0200

    exercise on bash env vars access

---
+++ assignments/20231013/scriprs/e2_reading_env.sh
@@ -0,0 +1,12 @@
+#Print value of HOME
+echo "home directory: $HOME"
+echo
+
+#Print vaule of PATH
+echo "path: $PATH"
+echo
+
+#Add custom dir to PATH
+echo "adding this script folder to env vars:"
+export BASH_SCRIPTS=./ #not sure abot this
+echo "added folder: $BASH_SCRIPTS"
