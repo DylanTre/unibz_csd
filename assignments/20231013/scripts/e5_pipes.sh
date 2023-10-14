89101a182f15c598f46a2284b76147a1bda83622
Dylan Trenti <dylan.trenti@marconirovereto.it>
2023-10-14 11:05:26 +0200

    exercise bash pipes

---
+++ assignments/20231013/scripts/e5_pipes.sh
@@ -0,0 +1,11 @@
+#piping ls and sort
+echo "executing ls | sort"
+echo
+ls | sort
+echo
+
+#piping echo and wc
+echo "executing wc on this sentence:"
+echo
+echo "executing wc on this sentence:" | wc
+
